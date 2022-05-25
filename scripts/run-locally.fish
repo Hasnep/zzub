4eb9266255115bd3643851c5d0286a4655e721cf
Hannes <h@nnes.dev>
2022-05-25 19:44:40 +0100

    WIP

--- scripts/run-locally.fish
+++
@@ -1,8 +0,0 @@
-set project_root $HOME/Documents/godot/jackbox-test
-
-black $project_root/docker/docker-compose.py \
-    && pipx run nginxfmt $project_root/docker/nginx.conf \
-    && python3 $project_root/docker/docker-compose.py $project_root/docker/docker-compose.yaml --local \
-    && docker-compose -f $project_root/docker/docker-compose.yaml build --build-arg SERVER_HOST=localhost --build-arg SERVER_PORT=5001 \
-    && docker-compose -f $project_root/docker/docker-compose.yaml up --remove-orphans
-rip $project_root/docker/docker-compose.yaml
