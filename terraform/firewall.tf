1d8e5001843c347d740e5119e2c3a428396166ea
Hannes <h@nnes.dev>
2022-05-26 22:24:35 +0100

    Uncomment most of the firewall configuration

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -1,81 +1,65 @@
-# # Firewall
-# resource "linode_firewall" "zzub" {
-#   label = "zzub"
-#   tags  = []
+resource "linode_firewall" "zzub" {
+  label   = "zzub"
+  linodes = [linode_instance.zzub.id]
+  tags    = []
 
-#   # Block by default
-#   # inbound_policy  = "DROP"
-#   # outbound_policy = "DROP"
+  # TODO: Enable firewall
+  inbound_policy  = "ACCEPT"
+  outbound_policy = "ACCEPT"
 
-#   # TODO: Enable firewall again
-#   inbound_policy  = "ACCEPT"
-#   outbound_policy = "ACCEPT"
+  # # Block by default
+  # inbound_policy  = "DROP"
+  # outbound_policy = "DROP"
 
-# #   # Allow HTTP connections
-# #   inbound {
-# #     label    = "http_inbound"
-# #     action   = "ACCEPT"
-# #     protocol = "TCP"
-# #     ports    = "80, 443"
-# #     ipv4     = ["0.0.0.0/0"]
+  # Allow HTTP connections
+  outbound {
+    label    = "http_inbound"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "80, 443"
+    ipv4     = ["0.0.0.0/0"]
+  }
 
-# #   }
+  # Allow inbound DNS connections
+  inbound {
+    label    = "dns_inbound"
+    action   = "ACCEPT"
+    protocol = "UDP"
+    ports    = "53"
+    ipv4     = ["0.0.0.0/0"]
+  }
 
-# #   outbound {
-# #     label    = "http_outbound"
-# #     action   = "ACCEPT"
-# #     protocol = "TCP"
-# #     ports    = "80, 443"
-# #     ipv4     = ["0.0.0.0/0"]
+  # Allow SSH connections
+  inbound {
+    label    = "ssh_inbound"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "22"
+    ipv4     = ["0.0.0.0/0"]
+  }
 
-# #   }
+  outbound {
+    label    = "ssh_outbound"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "22"
+    ipv4     = ["0.0.0.0/0"]
+  }
 
-# #   # Allow inbound DNS connections
-# #   inbound {
-# #     label    = "dns_inbound"
-# #     action   = "ACCEPT"
-# #     protocol = "UDP"
-# #     ports    = "53"
-# #     ipv4     = ["0.0.0.0/0"]
+  # Allow Websocket connections
+  inbound {
+    label    = "websocket_inbound"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "5001"
+    ipv4     = ["0.0.0.0/0"]
+  }
 
-# #   }
-
-# #   # Allow SSH connections
-# #   inbound {
-# #     label    = "ssh_inbound"
-# #     action   = "ACCEPT"
-# #     protocol = "TCP"
-# #     ports    = "22"
-# #     ipv4     = ["0.0.0.0/0"]
-
-# #   }
-
-# #   outbound {
-# #     label    = "ssh_outbound"
-# #     action   = "ACCEPT"
-# #     protocol = "TCP"
-# #     ports    = "22"
-# #     ipv4     = ["0.0.0.0/0"]
-
-# #   }
-
-# #   # Allow traffic for Minecraft server
-# #   inbound {
-# #     label    = "minecraft_inbound"
-# #     action   = "ACCEPT"
-# #     protocol = "TCP"
-# #     ports    = "25565"
-# #     ipv4     = ["0.0.0.0/0"]
-
-# #   }
-
-# #   outbound {
-# #     label    = "minecraft_outbound"
-# #     action   = "ACCEPT"
-# #     protocol = "TCP"
-# #     ports    = "25565"
-# #     ipv4     = ["0.0.0.0/0"]
-# #   }
-
-#   linodes = [linode_instance.zzub.id]
-# }
+  outbound {
+    label    = "websocket_outbound"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "5001"
+    ipv4     = ["0.0.0.0/0"]
+  }
+}
